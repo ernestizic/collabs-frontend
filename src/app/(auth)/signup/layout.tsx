0a3e3a15abbbc2beb0bf102c8dc878a5e1d3ff5f
ernestizic <ieifeanyichukwu@gmail.com>
2025-10-04 13:47:04 +0100

    UI: add signup csreen

--- src/app/(auth)/signup/layout.tsx
+++ src/app/(auth)/signup/layout.tsx
@@ -1,9 +1,49 @@
-const SignupLayout = ({children}: {children: React.ReactNode}) => {
-  return ( 
-    <div className="border-2">
-      {children}
-    </div>
-   );
-}
- 
+const SignupLayout = ({ children }: { children: React.ReactNode }) => {
+	const icons = ["📊", "📈", "📉", "🔗", "📊", "📈", "📉", "🔗", "📧"];
+	return (
+		<div className="bg-[#fff] h-screen flex p-4 2xl:p-8">
+			<div className="w-[40%] h-full overflow-auto">{children}</div>
+			<div className="flex-1 flex items-center justify-center bg-gradient-to-b from-[#20012f] to-[#f5b2e9] border-6 border-[#20012f] rounded-4xl text-white">
+				<div className="w-3/4 text-center">
+					<span className="block h-[2px] w-[300px] bg-[#fff] mx-auto mb-8" />
+					<p className="text-3xl font-semibold mb-[4px]">
+						Seamless collaboration
+					</p>
+					<p>Get the features you need to sync with your team</p>
+
+					<div className="overflow-hidden h-[270px] relative flex items-center justify-center">
+						{/* Center Circle */}
+						<div className="size-20 mt-40 rounded-full bg-[#B79785] flex items-center justify-center shadow-xl z-10">
+							<span className="text-4xl text-white">🧑‍🤝‍🧑</span>
+						</div>
+
+						{/* Orbit Container */}
+						<div className="absolute mt-75 w-[600px] h-[300px] animate-[spin_20s_linear_infinite]">
+							{icons.map((icon, index) => {
+								const angle = (index / icons.length) * 360;
+								return (
+									<div
+										key={index}
+										className="absolute w-16 h-16 rounded-full bg-white flex items-center justify-center shadow-lg"
+										style={{
+											transform: `rotate(${angle}deg) translate(190px) rotate(-${angle}deg)`,
+											top: "50%",
+											left: "50%",
+											margin: "-32px",
+										}}
+									>
+										<span className="text-2xl">{icon}</span>
+									</div>
+								);
+							})}
+						</div>
+						{/* Bottom Fade */}
+						<div className="max-2xl:hidden absolute bottom-0 w-full h-24 bg-gradient-to-t from-[#b578b5] via-[#b578b5]/5 to-transparent pointer-events-none" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
 export default SignupLayout;
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,151 @@
+"use client";
+
+import z from "zod";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff, Lock, MailIcon } from "lucide-react";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import {
+	InputGroup,
+	InputGroupAddon,
+	InputGroupButton,
+	InputGroupInput,
+} from "@/components/ui/input-group";
+import { Input } from "@/components/ui/input";
+
 const SignupPage = () => {
-  return ( 
-    <div>
-      <p>Sign up</p>
-    </div>
-   );
-}
- 
+	const [showPassword, setShowPassword] = useState<boolean>(false);
+
+	const formSchema = z.object({
+		firstname: z
+			.string()
+			.min(2, "Firstname should be at least 2 character long"),
+		lastname: z.string().min(2, "Lastname should be at least 2 character long"),
+		email: z.email().min(1, "Email is required"),
+		password: z.string().min(8, "Password must be at least 8 characters"),
+	});
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			firstname: "",
+			lastname: "",
+			email: "",
+			password: "",
+		},
+	});
+
+	const handleSubmit = (values: z.infer<typeof formSchema>) => {
+		console.log(values);
+	};
+	return (
+		<div className="h-full max-2xl:my-10 flex items-center justify-center pr-4 2xl:pr-8">
+			<div className="w-3/4 2xl:w-2/4">
+				<div className="mb-10">
+					<p className="text-4xl font-bold mb-[6px]">
+						Let&apos;s get you set up
+					</p>
+				</div>
+
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(handleSubmit)}>
+						<div className="space-y-8">
+							<FormField
+								control={form.control}
+								name="firstname"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Firstname</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="lastname"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Lastname</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Email</FormLabel>
+										<FormControl>
+											<InputGroup>
+												<InputGroupInput type="email" {...field} />
+												<InputGroupAddon>
+													<MailIcon />
+												</InputGroupAddon>
+											</InputGroup>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Password</FormLabel>
+										<FormControl>
+											<InputGroup>
+												<InputGroupInput
+													type={showPassword ? "text" : "password"}
+													{...field}
+												/>
+												<InputGroupAddon>
+													<Lock />
+												</InputGroupAddon>
+												<InputGroupAddon align="inline-end">
+													<InputGroupButton
+														onClick={() => setShowPassword((prev) => !prev)}
+													>
+														{showPassword ? <EyeOff /> : <Eye />}
+													</InputGroupButton>
+												</InputGroupAddon>
+											</InputGroup>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</div>
+						<Button type="submit" className="w-full mt-8 h-10">
+							Submit
+						</Button>
+					</form>
+				</Form>
+
+				<p className="text-center mt-4">
+					Already have an account?{" "}
+					<Link href="/login" className="underline">Login instead</Link>
+				</p>
+			</div>
+		</div>
+	);
+};
+
 export default SignupPage;
